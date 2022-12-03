f3dec014521a5ff74c3469eab1fb29cd03528f1b
NelsonZeng25 <nelson.zeng25@gmail.com>
2022-12-03 03:26:02 -0500

    Fixed the BackendApplicationTests and gradle build by adding a test profile which specifies the in memory hsqldb database used for tests

--- Backend/src/test/java/ca/mcgill/ecse437/studentsystem/BackendApplicationTests.java
+++ Backend/src/test/java/ca/mcgill/ecse437/studentsystem/BackendApplicationTests.java
@@ -1,13 +1,17 @@
 package ca.mcgill.ecse437.studentsystem;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest(classes = BackendApplication.class)
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(BackendApplication.class);
 	}
 
 }
--- Backend/src/test/java/ca/mcgill/ecse437/studentsystem/repository/StudentRepositoryTests.java
+++ Backend/src/test/java/ca/mcgill/ecse437/studentsystem/repository/StudentRepositoryTests.java
@@ -5,15 +5,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@ActiveProfiles("test")
 @DataJpaTest
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.HSQLDB)
 public class StudentRepositoryTests {
 
     @Autowired
